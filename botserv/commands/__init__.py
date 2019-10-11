e0c76c22e827dcc70d5070773fba217b5c6fdb8c
Blondel MONDESIR <16546989+deldesir@users.noreply.github.com>
2019-10-11 11:58:27 -0500

    Create __init__.py

---
+++ botserv/commands/__init__.py
@@ -0,0 +1,19 @@
+from botserv import LOGS
+
+
+def __list_all_commands():
+    from os.path import dirname, basename, isfile
+    import glob
+
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_commands = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_commands
+
+
+ALL_COMMANDS = sorted(__list_all_commands())
+LOGS.info("Modules to load:\n{}\n".format(", ".join(ALL_COMMANDS)))
+__all__ = ALL_COMMANDS + ["ALL_COMMANDS"]
