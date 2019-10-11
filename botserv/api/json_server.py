3145879b0a47c5e6e09489e9a6aa5600d1a5cd86
Blondel MONDESIR <16546989+deldesir@users.noreply.github.com>
2019-10-10 19:38:05 -0500

    Update json_server.py

--- botserv/api/json_server.py
+++ botserv/api/json_server.py
@@ -1,1 +1,75 @@
+#!/usr/bin/env python
 
+# Manipulate sys.path to be able to import rivescript from this local git
+# repository.
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+from flask import Flask, request, Response, jsonify
+import json
+from rivescript import RiveScript
+from redis_storage import RedisSessionStorage
+
+bot = RiveScript(
+    session_manager=RedisSessionStorage()
+)
+bot.load_directory(
+    os.path.join(os.path.dirname(__file__), "..", "brain")
+)
+bot.sort_replies()
+
+app = Flask(__name__)
+
+@app.route("/reply", methods=["POST"])
+def reply():
+    """Fetch a reply from RiveScript.
+
+    Parameters (JSON):
+    * username
+    * message
+    * vars
+    """
+    params = request.json
+    if not params:
+        return jsonify({
+            "status": "error",
+            "error": "Request must be of the application/json type!",
+        })
+
+    username = params.get("username")
+    message  = params.get("message")
+#   uservars = params.get("vars", dict())
+
+    # Make sure the required params are present.
+    if username is None or message is None:
+        return jsonify({
+            "status": "error",
+            "error": "username and message are required keys",
+        })
+
+    # Get a reply from the bot.
+    reply = bot.reply(username, message)
+
+    # Send the response.
+    return reply 
+
+@app.route("/")
+@app.route("/<path:path>")
+def index(path=None):
+    """On all other routes, just return an example `curl` command."""
+    payload = {
+        "username": "soandso",
+        "message": "Hello bot",
+        "vars": {
+            "name": "Soandso",
+        }
+    }
+    return Response(r"""Usage: curl -i \
+    -H "Content-Type: application/json" \
+    -X POST -d '{}' \
+    http://localhost:5000/reply""".format(json.dumps(payload)),
+    mimetype="text/plain")
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', debug=False)
